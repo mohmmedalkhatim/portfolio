710c7d7a4c7dc6e693e2abcc480eb91247d060ed
mohmmedalkhatm <evalotinoncalculation@gmail.com>
2024-08-16 09:23:37 +0200

    redesign the project proccess

--- src/components/proccess.tsx
+++ src/components/proccess.tsx
@@ -1,55 +1,52 @@
-import React from 'react'
-import { TbArrowDown, TbArrowRight, TbColorPicker, TbCube, TbDatabase, TbLamp, TbLamp2, TbRepeat, TbTestPipe, TbUpload } from 'react-icons/tb'
+import { Textarea } from "@headlessui/react"
+
 
 export const Process = () => {
   return (
     <section className='process'>
-      <div className="item">
-      <div className='flex items-center gap-5'>
-          <TbColorPicker size={"1.7rem"} />
-          <div className='text-xl'>
-            desgin
+      <div className="item1 flex pl-8">
+        <div className="anime">
+          <h2 className="ml-4 text-3xl leading-loose mb-4">Define project goals, target audience, and scope.</h2>
+          <div className="flex flex-col gap-8">
+            <div className="inline-flex gap-2">
+              <div className="flex">
+                <div className="rounded mt-3 border-[0.16rem] w-0 inlin"></div>
+              </div>
+              <p className="pr-4 w-[40rem] text-md p-4">
+              The Planning & Design phase is where the foundation of the web project is established.
+                This step involves defining the project's goals, understanding the target audience,
+                and determining the scope to ensure the project stays focused and aligned with the
+                client's needs. Once the objectives are clear, wireframes are created to visualize
+                the structure and flow of the website. This is followed by designing the user interface,
+                ensuring that the site is both aesthetically pleasing and user-friendly, setting the stage
+                for a successful development process.
+              </p>
+            </div>
+            <Textarea cols={4} className={"border p-3 rounded-md text-[#e5e7eb] border-[#e1e1e140] placeholder:text-[#e5e7eb] placeholder:opacity-50 focus:outline-none placeholder:text-sm  bg-[#22272e] "} placeholder="writh your goals...">
+
+            </Textarea>
           </div>
         </div>
+        <img src="/icons/frist.svg" width={600} alt="" className="anime p-12" />
       </div>
-      <div className="arrow"><TbArrowRight size={"1.7rem"} /></div>
-      <div className="item">
-      <div className='flex items-center gap-5'>
-          <TbCube size={"1.7rem"} />
-          <div className='text-xl'>
-            build
+      <div className="item1 flex pl-8">
+        <div className="anime">
+          <h2 className="ml-4 text-3xl leading-loose mb-4">Define project goals, target audience, and scope.</h2>
+          <div className="flex flex-col gap-8">
+            <div className="inline-flex gap-2">
+              <div className="flex">
+                <div className="rounded mt-3 border-[0.16rem] w-0 inlin"></div>
+              </div>
+              <p className="pr-4 w-[40rem] text-md p-4">
+
+              </p>
+            </div>
+            <Textarea cols={4} className={"border p-3 rounded-md text-[#e5e7eb] border-[#e1e1e140] placeholder:text-[#e5e7eb] placeholder:opacity-50 focus:outline-none placeholder:text-sm  bg-[#22272e] "} placeholder="writh your goals...">
+
+            </Textarea>
           </div>
         </div>
-      </div>
-      <div className="arrow"><TbArrowRight size={"1.7rem"} /></div>
-      <div className="item">
-      <div className='flex items-center gap-5'>
-          <TbTestPipe />
-          <div>
-            Test
-          </div>
-        </div>
-      </div>
-      <div className="arrow"><TbArrowRight size={"1.7rem"} /></div>
-      <div className="item">
-      <div className='flex items-center gap-5'>
-          <TbRepeat />
-          <div>
-            Repeat
-          </div>
-        </div>
-      </div>
-      <div className="arrow"><TbArrowRight size={"1.7rem"} /></div>
-      <div className="item">
-      <div className='flex items-center gap-5'>
-          <TbUpload />
-          <div>
-            deploy
-          </div>
-        </div>
-      </div>
-      <div className="arrow">
-        <TbArrowDown />
+        <img src="/icons/second.svg" width={600} alt="" className="anime p-12" />
       </div>
     </section>
   )
