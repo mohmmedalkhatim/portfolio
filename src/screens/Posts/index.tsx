23bd0d30ed59be6176860a20c665de19526a6d9b
mohmmedalkhatim <developmentaccun@outlook.com>
2025-02-17 08:56:38 +0200

    some work

--- src/screens/Posts/index.tsx
+++ src/screens/Posts/index.tsx
@@ -1,13 +1,19 @@
 import { usePosts } from "../../context/posts"
 
+
+
+
 function Posts() {
-  let {list} = usePosts()
+  let list = usePosts((post) => post.list)
   return (
     <main>
       <ul>
-        
+        {list.map(item =>
+            <li>
+              <h3>{item.title}</h3>
+            </li>
+        )}
       </ul>
-        
     </main>
   )
 }
