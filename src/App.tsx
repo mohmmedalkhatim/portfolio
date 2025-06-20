8277086a102fa6f4c5fa984e3aa87c55ca28dfdb
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-20 10:48:46 +0200

    adding the dialog to the the main app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import './index.css';
+import { useState } from 'react';
+import Dialog from './components/dialog';
 function App() {
+  let [dialogState,setDialogState] = useState(false)
   return (
     <>
-      <Header />
+      <Header openDialog={setDialogState}/>
       <Navbar />
       <Outlet />
+      <Dialog state={dialogState} setState={setDialogState}/>
     </>
   );
 }
