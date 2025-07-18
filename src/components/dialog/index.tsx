05a905479a367594f6805ac8ac6817b94ca9434c
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-07-18 13:53:20 +0200

    adding list to the dialog

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -1,5 +1,12 @@
 import React, { Dispatch } from 'react';
 import { TbX } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
+
+const routes = [
+  { name: 'main', route: '/' },
+  { name: 'projects', route: '/projects' },
+  { name: 'contacts', route: '/contacts' },
+];
 
 function Dialog ({
   state,
@@ -13,8 +20,13 @@ function Dialog ({
       className='fixed top-0 right-8 z-50 bg-slate-800 w-full h-screen transition-[right] duration-300'
       style={{ right: state ? '0' : '-100%' }}
     >
-      <div className='absolute top-8 right-8 ' onClick={()=>setState(!state)}>
-        <TbX  size={"2rem"}/>
+      <div className='pt-20 flex flex-col'>
+        {routes.map(item => (
+          <Link className='py-4 px-4 hover:bg-gray-500' to={item.route}>{item.name}</Link>
+        ))}
+      </div>
+      <div className='absolute top-8 right-8 ' onClick={() => setState(!state)}>
+        <TbX size={'2rem'} />
       </div>
     </div>
   );
